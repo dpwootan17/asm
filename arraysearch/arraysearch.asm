38c39796627d53f76e666fa83b5d3046b11e0822
David Wootan <dwootan@gmail.com>
2015-07-03 20:45:40 -0700

    initial changes

--- arraysearch/arraysearch.asm
+++ arraysearch/arraysearch.asm
@@ -5,17 +5,22 @@
 
     .data                           # section declaration
 
-msg:
-    .ascii      "Hello, world!\n"   # our dear string
-    .global _start                  # ELF linker requires starting point to be _startr 
+	msg: 		.ascii  "Hello, world!\n"   # constant string buffer
+	arr_bf:		.space  0x400			# arrarybuffer
+
+    .global _start                  # ELF linker requires starting point to be _startr
+
 
 _start:
-	  mov   eax,SYS_WRITE       # system call number (sys_write)
-	  mov   ebx,1               # first argument: file handle (stdout)
-	  mov   ecx,offset msg      # second argument: pointer to message to write
-	  mov   edx,14              # third argument: message length
-	  int   0x80                # call kernel
+	/* generate random stuff */
 
-	  mov   ebx,SYS_EXIT        # first argument: exit code
-	  mov   eax,1               # system call number (sys_exit)
-	  int   0x80                # call kernel
+
+    mov   eax,SYS_WRITE             # system call number (sys_write)
+    mov   ebx,1                     # first argument: file handle (stdout)
+    mov   ecx,offset msg            # second argument: pointer to message to write
+    mov   edx,14                    # third argument: message length
+    int   0x80                      # call kernel
+
+    mov   ebx,SYS_EXIT              # first argument: exit code
+    mov   eax,1                     # system call number (sys_exit)
+    int   0x80                      # call kernel
