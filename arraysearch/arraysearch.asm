ae6e74b0f9d43474083eb36471e441f85229a332
David Wootan <dwootan@gmail.com>
2015-07-19 16:40:00 -0700

    new code and pdf

--- arraysearch/arraysearch.asm
+++ arraysearch/arraysearch.asm
@@ -3,14 +3,23 @@
     .include "../includes/unistd.h"
 
 .data
-    bitches:    .ascii  "bitches.....\n"
+    adjup:    .ascii  "\nadjup\n"
+    uplen =.-adjup
+    adjdown:    .ascii  "\nadjdown\n"
+    downlen =.-adjdown
+
+    bitches:    .ascii  "\nbitches.....\n"
     blen =.-bitches
     msg_seed:   .ascii  "seed generation was successful\n"
     slen = .-msg_seed
-    errfail:    .ascii  "*** Failure ***\n"
+    errfail:    .ascii  "\n*** Failure ***\n"
     elen =.-errfail
 
-    arrbf:      .space  0x400,'#'   # array buffer
+    arrbf:      .asciz  ""          # array buffer
+        .org arrbf + 0x400
+    regdata:    .asciz  ""
+        .org regdata + 4
+
     msgptr:     .int    0           # ptr to msg buffer
     msglen:     .int    0           # length of the msg buffer data
     seed:       .int    0           # seed for the random number generator
@@ -24,38 +33,80 @@ _start:
     jmp     exit
 
 fillbuffer:
-    mov     ecx, 0x100              # initialize loop counters
-    mov     edx, 0
-    _loopstart:
-        push    ecx                 # push ecx
-        push    edx                 # push edx
-        call    setseed
-        pop     edx                 # pop  edx
 
-        mov     eax, 8              # multiplier
-        mov     ecx, cntr           # multiplicand
-        mul     ecx                 # result will be in EDX:EAX
-        mov     esi, eax                                # put result in esi
-        mov     edi, offset dword ptr arrbf[esi]        # point at arrbf
+     call    setseed             # get new seed
 
-        mov     esi, seed           # set to point at seed
-        pop     ecx                 # pop ecx loop counter`
-        xchg    eax,ecx             # and save it again
-        mov     ecx, 2              # set ecx to move two dwords during the movsb
-        cld                         # new string op, clear the direction flag
-#       rep     movsd               # move the data
-        xchg    eax,ecx             # restore ecx one more time
+/* */
 
-        inc     dword ptr cntr      # increment cntr for next arrbf[offset] calculation
-    loopnz _loopstart
+    mov ecx, 4
+    cld
+    mov esi, offset dword ptr seed
+    mov edi, offset dword ptr regdata
+    rep movsb
+
+    mov     msgptr, offset dword ptr regdata
+    mov     msglen, dword ptr 4
+    call    print
+
+    sub edi, edi            # clear registers
+    sub ecx, ecx
+    mov esi, regdata[ecx]   # point si at the next char
+    mov edi, 0x20           # set edi to space char
+    cld
+    cmp esi, edi            # do the compare
+    jbe _adjustdown         # if esi <= edi, add value to regdata
+    mov edi, 0x7e           # set edi to tilde char
+    cld
+    cmp esi, edi            # compare again
+    jg  _adjustup           # if esi > edi, subtract value in regdata
+    jmp _donehere
+
+_adjustdown:
+    mov     msgptr, offset dword ptr adjdown
+    mov     msglen, dword ptr downlen
+    call    print
+#    mov [esi], dword ptr 0x7e         # set tilde char
+
+_adjustup:
+    mov     msgptr, offset dword ptr adjup
+    mov     msglen, dword ptr uplen
+    call    print
+#    mov     [esi], dword ptr 0x32         # add value of space char to the buffer
+
+_donehere:
 
     mov     msgptr, offset dword ptr bitches
     mov     msglen, dword ptr blen
     call    print
 
-    sub     esi, esi
-    mov     msgptr, offset dword ptr arrbf
-    mov     msglen, offset dword ptr dlen
+    mov     msgptr, esi
+    mov     msglen, dword ptr 4
+    call    print
+
+
+    mov     msgptr, offset dword ptr bitches
+    mov     msglen, dword ptr blen
+    call    print
+
+    mov     eax,SYS_WRITE                  # system call number (sys_write)
+    mov     ebx,1                          # first argument: file handle (stdout)
+    mov     ecx,offset dword ptr regdata   # second argument: pointer to message to write */
+    mov     edx,4                          # third argument: message length
+    int     0x80                           # call kernel
+
+/* */
+
+#        mov     eax, 8                              # multiplier
+#        mov     ecx, cntr                           # multiplicand
+#        mul     ecx                                 # result will be in EDX:EAX
+#        mov     esi, eax                            # put result in esi
+#        mov     edi, offset dword ptr arrbf[esi]    # point at arrbf
+#
+#        inc     dword ptr cntr                      # increment cntr for next arrbf[offset] calculation
+
+_end:
+    mov     msgptr, offset dword ptr bitches
+    mov     msglen, dword ptr blen
     call    print
 
     ret
